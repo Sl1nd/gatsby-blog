878c9403c5f9cbfc84556a4c6929ddbce66c8ec7
Slindauer <slindauer@Slindauers-MacBook-Pro.local>
2019-11-12 11:47:36 +0100

    import  react-markdown to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
+import ReactMarkdown from "react-markdown"
 import "../styles/global.css"  
 
 const IndexPage = ({data}) => (
